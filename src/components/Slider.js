8fec9110a69cdc94b24f00b94c9692e4338e6b29
user <user@itoour.com>
2020-09-07 11:39:07 -0500

    update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,21 +2,23 @@ import React, { Fragment } from "react";
 import { Carousel } from "react-bootstrap";
 import c1 from "../assets/c1.png";
 import c2 from "../assets/c2.jpg";
+import header1 from "../assets/header1.jpg";
+import header2 from "../assets/header2.jpg";
 import "./css/Slider.css";
 
 export default function Slider() {
   return (
     <Fragment>
       <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src={c1} alt="..." />
+        <Carousel.Item style={{maxHeight: '768px'}}>
+          <img className="d-block w-100" src={header1} alt="..." />
           <Carousel.Caption>
             <h3>Bienvenid@</h3>
             <p>Nombre Apellido</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={c2} alt="..." />
+        <Carousel.Item style={{maxHeight: '768px'}}>
+          <img className="d-block w-100" src={header2} alt="..." />
           <Carousel.Caption>
             <h3>Gracias!</h3>
             <p>Por compartir este momento!</p>
