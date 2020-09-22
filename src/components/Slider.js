3e4c8bd9219cc66dd1bdce7c40d5cf6cc72bd37e
user <user@itoour.com>
2020-09-22 13:12:53 -0500

    update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,9 @@ import c1 from "../assets/c1.png";
 import c2 from "../assets/c2.jpg";
 import header1 from "../assets/header1.jpg";
 import header2 from "../assets/header2.jpg";
+import header3 from "../assets/header3.jpg";
+import header4 from "../assets/header4.jpg";
+import header5 from "../assets/header5.jpg";
 import "./css/Slider.css";
 
 export default function Slider() {
@@ -14,11 +17,32 @@ export default function Slider() {
           <img className="d-block w-100" src={header1} alt="..." />
           <Carousel.Caption>
             <h3>Bienvenid@</h3>
-            <p>Nombre Apellido</p>
+            {/* <p>Nombre Apellido</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={{maxHeight: '768px'}}>
           <img className="d-block w-100" src={header2} alt="..." />
+          <Carousel.Caption>
+            <h3>Gracias!</h3>
+            <p>Por compartir este momento!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{maxHeight: '768px'}}>
+          <img className="d-block w-100" src={header3} alt="..." />
+          <Carousel.Caption>
+            <h3>Gracias!</h3>
+            <p>Por compartir este momento!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{maxHeight: '768px'}}>
+          <img className="d-block w-100" src={header4} alt="..." />
+          <Carousel.Caption>
+            <h3>Gracias!</h3>
+            <p>Por compartir este momento!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{maxHeight: '768px'}}>
+          <img className="d-block w-100" src={header5} alt="..." />
           <Carousel.Caption>
             <h3>Gracias!</h3>
             <p>Por compartir este momento!</p>
